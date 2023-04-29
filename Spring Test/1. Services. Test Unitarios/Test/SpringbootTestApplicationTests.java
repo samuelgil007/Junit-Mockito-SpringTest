dc4bcf6af7277bfbef37f9f44d881c60a3643d69
Samuel Gil <53627477+samuelgil007@users.noreply.github.com>
2023-04-29 13:08:17 -0500

    Update SpringbootTestApplicationTests.java

--- Spring Test/1. Services. Test Unitarios/Test/SpringbootTestApplicationTests.java
+++ Spring Test/1. Services. Test Unitarios/Test/SpringbootTestApplicationTests.java
@@ -126,4 +126,41 @@ class SpringbootTestApplicationTests {
 
 		verify(cuentaRepository, times(2)).findById(1L);
 	}
+	
+	@Test
+	void testFindAll() {
+		// Given
+		List<Cuenta> datos = Arrays.asList(crearCuenta001().orElseThrow(), crearCuenta002().orElseThrow());
+		when(cuentaRepository.findAll()).thenReturn(datos);
+
+		// when
+		List<Cuenta> cuentas = service.findAll();
+
+		// then
+		assertFalse(cuentas.isEmpty());
+		assertEquals(2, cuentas.size());
+		assertTrue(cuentas.contains(crearCuenta002().orElseThrow()));
+
+		verify(cuentaRepository).findAll();
+	}
+
+	@Test
+	void testSave() {
+		// given
+		Cuenta cuentaPepe = new Cuenta(null, "Pepe", new BigDecimal("3000"));
+		when(cuentaRepository.save(any())).then(invocation ->{
+			Cuenta c = invocation.getArgument(0);
+			c.setId(3L);
+			return c;
+		});
+
+		// when
+		Cuenta cuenta = service.save(cuentaPepe);
+		// then
+		assertEquals("Pepe", cuenta.getPersona());
+		assertEquals(3, cuenta.getId());
+		assertEquals("3000", cuenta.getSaldo().toPlainString());
+
+		verify(cuentaRepository).save(any());
+	}
 }
